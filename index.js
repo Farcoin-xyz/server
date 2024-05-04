665d8732f5cd624d44343932873787469eba5a08
0xfc002 <farcoin_xyz@protonmail.com>
2024-05-03 21:51:14 -0400

    Add fallback route

--- index.js
+++ index.js
@@ -375,6 +375,10 @@ app.get('/scan', async (req, res) => {
   }
 });
 
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build/index.html'));
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
